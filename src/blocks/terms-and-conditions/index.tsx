2c07187a3714f0973d9e22f44a04a9bcd27a3eab
Loïc <loiczeiss@gmail.com>
2025-07-07 17:31:15 +0200

    Feat: - Add TermsAndConditions component with basic structure [wip]
    Introduce a new `TermsAndConditions` component containing a basic layout with placeholders for content. This serves as an initial setup for implementing terms and conditions UI in the application.

---
+++ src/blocks/terms-and-conditions/index.tsx
@@ -0,0 +1,11 @@
+export function TermsAndConditions() {
+    return (
+        <div className="flex min-h-screen flex-col items-center justify-center space-y-6 p-4">
+            <p className={'font-bold4 text-4xl'}></p>
+            <div className={'flex flex-col items-center justify-center space-y-4'}>
+                <p className={'text-xl font-semibold'}></p>
+                <p className={'text-base'}></p>
+            </div>
+        </div>
+    );
+}
